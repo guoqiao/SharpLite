435e5b17427bbc0692d879c1f83aa09249b8e5b4
guoqiao <guoqiao@gmail.com>
2012-05-01 13:01:47 +0800

    add max_length to model

--- django-orm/main/models.py
+++ django-orm/main/models.py
@@ -4,8 +4,8 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField()
-    password = models.CharField()
+    username = models.CharField(default="", max_length=63)
+    password = models.CharField(default="", max_length=63)
     
     class Meta:
         db_table = 'User'
